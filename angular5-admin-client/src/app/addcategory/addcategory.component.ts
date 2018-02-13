1b615418eb90f70f62b62a6e8ce9f1899bc4b551
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-02-13 16:14:15 +0530

    Added Angular Reactive Form Technique  Add Category

--- angular5-admin-client/src/app/addcategory/addcategory.component.ts
+++ angular5-admin-client/src/app/addcategory/addcategory.component.ts
@@ -1,15 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, OnChanges } from '@angular/core';
+import { Category } from '../models/category.model';
+import { FormControl, FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-addcategory',
   templateUrl: './addcategory.component.html',
   styleUrls: ['./addcategory.component.css']
 })
-export class AddcategoryComponent implements OnInit {
+export class AddcategoryComponent implements OnInit ,OnChanges {
+  catgrFormGeneral:FormGroup;
+  catgrFormData:FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb:FormBuilder
+  ){
+    this.createForm();
+   }
+
+  createForm(){
+    this.catgrFormGeneral = this.fb.group({
+      name:['', Validators.required],
+      desciption:'',
+      meta_title:['', Validators.required],
+      meta_description:'',
+      meta_keyword:''
+    });
+    this.catgrFormData = this.fb.group({
+      parent:''
+    })
+  }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(){
+
   }
 
 }
