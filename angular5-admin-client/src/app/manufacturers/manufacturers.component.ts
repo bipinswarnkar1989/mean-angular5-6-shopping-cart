39e1c44e48e09f3bba5b390dc28e957c97d25d51
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-02-22 20:15:08 +0530

    Added Basic View in Manufacturers Component

--- angular5-admin-client/src/app/manufacturers/manufacturers.component.ts
+++ angular5-admin-client/src/app/manufacturers/manufacturers.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-manufacturers',
   templateUrl: './manufacturers.component.html',
   styleUrls: ['./manufacturers.component.css']
 })
 export class ManufacturersComponent implements OnInit {
+  searchManftrField:FormControl;
 
   constructor() { }
 
   ngOnInit() {
+    this.searchManftrField = new FormControl();
   }
 
 }
