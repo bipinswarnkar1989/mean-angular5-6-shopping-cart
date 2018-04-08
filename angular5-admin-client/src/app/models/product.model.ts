47df83523f0034f452976b240fed9279ebd5499f
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-04-08 20:06:24 +0530

    Added Product Model

---
+++ angular5-admin-client/src/app/models/product.model.ts
@@ -0,0 +1,12 @@
+export interface Product {
+    name:string,
+    slug?:string,
+    description?:string,
+    meta_title?:string,
+    meta_description?:string,
+    meta_keywords?:string,
+    tags?:string,
+    model?:string,
+    createdAt:string,
+    updatedAt:string
+}
