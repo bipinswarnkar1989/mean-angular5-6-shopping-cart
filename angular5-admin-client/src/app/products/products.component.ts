56f22528a868f4a81744622a486e03fcc37d7288
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-04-10 20:52:55 +0530

    Added Get Products in Angular Client

--- angular5-admin-client/src/app/products/products.component.ts
+++ angular5-admin-client/src/app/products/products.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../models/product.model';
 import {MatTableDataSource} from '@angular/material';
 import {SelectionModel} from '@angular/cdk/collections';
+import { ProductService } from "../services/product.service";
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-products',
@@ -14,9 +16,29 @@ export class ProductsComponent implements OnInit {
   products: Product[];
   selection = new SelectionModel<Product>(true, []);
   dataSource = new MatTableDataSource<Product>(this.products);
-  constructor() { }
+
+  constructor(
+    private productService:ProductService,
+    private route:ActivatedRoute,
+    private router:Router
+  ) { }
 
   ngOnInit() {
+    this.route.params.subscribe(params => this.getProducts(params));
+  }
+
+  getProducts(params){
+    this.productService.getProducts(params)
+                       .subscribe(
+                         data => {
+                           console.log(data)
+                           this.products = data['products'];
+                           this.dataSource = new MatTableDataSource<Product>(this.products);
+                         },
+                         error => {
+                           console.log(error);
+                         }
+                       )
   }
 
   /** Whether the number of selected elements matches the total number of rows. */
--- angular5-admin-client/src/app/services/product.service.ts
+++ angular5-admin-client/src/app/services/product.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders, HttpParams } from "@angular/common/http";
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class ProductService {
+  apiUrl = 'http://localhost:3001/api/product';
+  token = localStorage.getItem('userToken') || null;
+  httpHeaders:HttpHeaders = new HttpHeaders();
+  constructor(
+    private http:HttpClient
+  ) { }
 
-  constructor() { }
+  getProducts(params):Observable<any>{
+    return this.http.get(`${this.apiUrl}/${params.page}/${params.limit}`, {
+      headers:this.httpHeaders.set('authorization', this.token)
+    })
+  }
 
 }
